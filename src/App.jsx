123245486bbe7518a4d35975fef967f7ebef54ac
nakamotokanako-coder <nakamotokanako@gmail.com>
2026-06-05 19:24:25 +0900

    Add Z to board header logo

--- src/App.jsx
+++ src/App.jsx
@@ -161,7 +161,7 @@ export default function App() {
             <span className="brand-mark">遁</span>
             <div>
               <span className="board-kicker lat">KIMON TONKO</span>
-              <h1 className="maru">奇門遁甲</h1>
+              <h1 className="maru">奇門遁甲<span className="brand-z metal lat">Z</span></h1>
             </div>
           </div>
         </div>
